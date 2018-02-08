c7a399a7ebf0bb5944388d57e64b0b7e4388dc04
Roman Bumerl-Lexa <roman@bumerl.at>
2018-02-08 21:59:49 +0100

    added software-uart for usage with nano

--- ReadCO2.ino
+++ ReadCO2.ino
@@ -1,45 +1,40 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+char readbyte[9] = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial1.begin(9600);
+  //Serial1.begin(9600);
+  mySerial.begin(9600);
 }
 
 void loop() {
 
-  while(Serial1.available()) Serial1.read(); // clear buffer before starting communication
+  while(mySerial.available()) mySerial.read(); // clear buffer before starting communication
 
-  Serial1.write(0xFF);  // start byte
-  Serial1.write(0x01);  // sensor ID
-  Serial1.write(0x86);  // 0x86: read CO2, 0x87: Calibrate zero point, 0x88 Calibrate span point
-  Serial1.write(0x00);
-  Serial1.write(0x00);
-  Serial1.write(0x00);
-  Serial1.write(0x00);
-  Serial1.write(0x00);
-  Serial1.write(0x79);  // checksum, see getChecksum.
+  for (int i=0;i<9;i++){
+    mySerial.write(readbyte[i]);
+  }
 
-  int timeout=100;
-  int count=9;
+  int timeout=100;  // no endless loop if no answer.
+  int count=9;  // we expect 9 Bytes as response
   unsigned char receivebuf[9];
   while(timeout&&count){
-    if (Serial1.available()) {
-      receivebuf[9-count] = Serial1.read();
+    if (mySerial.available()) {
+      receivebuf[9-count] = mySerial.read();
       count--;
     }  
   }
 
-    if(!(receivebuf[8]-getCheckSum(receivebuf))){
-    /*Serial.print("Byte3:");
-    Serial.print(receivebuf[2],DEC);
-    Serial.print("  Byte4:");
-    Serial.println(receivebuf[3],DEC);
-    Serial.print(receivebuf[8],HEX);
-    Serial.print("  ");
-    Serial.println(getCheckSum(receivebuf),HEX);*/
+  if(!(receivebuf[8]-getCheckSum(receivebuf))){ // checksum ok?
     Serial.println(receivebuf[2]*256+receivebuf[3],DEC);
   }
   else Serial.println("Data ERROR");
-  delay(1000);
+  delay(1000); 
 }
 
 unsigned char getCheckSum(unsigned char *packet)
